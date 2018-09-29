a8699e2e9e215ba8bd38c06169835d161f0bad1b
lee17026 <39200488+lee17026@users.noreply.github.com>
2018-09-29 23:42:44 +0000

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,10 +6,17 @@
     <title>Tim Lee's CS 313 Homepage</title>
     <meta name="description" content="The main hub for CS 313 assignments.">
     <link rel="stylesheet" href="main.css">
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   </head>
+    
   <body>
-    <h1>hey guys</h1>
-    <p>this is a p</p>
+    <!-- -->
+    <div class="jumbotron text-center">
+     <h1>Tim's CS 313 Home</h1>
+    </div>
+    <!-- Nav Bar -->
+    
       
   </body>
 </html>
