ccaf570537edb01cfc458e5ec877006b778e620a
lee17026 <39200488+lee17026@users.noreply.github.com>
2018-09-29 23:33:39 +0000

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,15 @@
-<?php
-
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+<html>
+    
+  <head>
+    <meta charset="utf-8">
+    <title>Tim Lee's CS 313 Homepage</title>
+    <meta name="description" content="The main hub for CS 313 assignments.">
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <h1>hey guys</h1>
+    <p>this is a p</p>
+      
+  </body>
+</html>
