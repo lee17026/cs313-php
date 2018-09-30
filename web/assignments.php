346fd6b969d919efd90af49e50625535e3300b3c
lee17026 <39200488+lee17026@users.noreply.github.com>
2018-09-30 01:27:19 +0000

    Create assignments.php

---
+++ web/assignments.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    
+  <head>
+    <meta charset="utf-8">
+    <title>Tim Lee's CS 313 Assignments</title>
+    <meta name="description" content="The main hub for CS 313 assignments.">      
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+  </head>
+  
+  <body>
+  <!-- Nav Bar -->
+    <nav class="navbar navbar-expand-sm bg-light justify-content-center">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Assignments</a>
+      </ul>
+    </nav>
+  </body>
+  
+</html>
