9fed0928bbfef435cd177327a439ea6a0d75072f
Tim Lee <lee17026@byui.edu>
2018-10-15 18:00:28 -0700

    created teach05.php

---
+++ web/week05/teach05.php
@@ -0,0 +1,48 @@
+<?php
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    
+  <head>
+    <meta charset="utf-8">
+    <title>Week 05 Team Activity</title>   
+  </head>
+  
+  <body>
+	<h1>For Each example</h1>
+	<?php
+	foreach ($db->query('SELECT * FROM public.scriptures') as $row)
+	{
+	  echo 'id: ' . $row['id'];
+	  echo ' book: ' . $row['book'];
+	  echo ' chapter: ' . $row['chapter'];
+	  echo ' verse: ' . $row['verse'];
+	  echo ' content: ' . $row['content'];
+	  echo '<br/>';
+	}
+	?>
+  </body>
+  
+ </html>
