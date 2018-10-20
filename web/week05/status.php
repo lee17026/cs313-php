ae44a41f5eef1220e38040e89d98048bd73592e6
Tim Lee <lee17026@byui.edu>
2018-10-20 12:07:32 -0700

    added live db connection and query into status.php

--- web/week05/status.php
+++ web/week05/status.php
@@ -1,9 +1,32 @@
 <?php
+/* dummy data for internal testing
 $query = array
   (
   array("silo_number" => "11", "amount" => 25000),
   array("silo_number" => "12", "amount" => 13679)
   );
+  */
+<?php
+try {
+	$dbUrl = getenv('DATABASE_URL');
+	
+	$dbOpts = parse_url($dbUrl);
+	
+	$dbHost = $dbOpts["host"];
+	$dbPort = $dbOpts["port"];
+	$dbUser = $dbOpts["user"];
+	$dbPassword = $dbOpts["pass"];
+	$dbName = ltrim($dbOpts["path"],'/');
+	
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $ex) {
+	$msg = $ex->getMessage();
+	echo "Error!: $msg";
+	die();
+}
+?>
 ?>
 <!DOCTYPE html>
 <html lang="en-US">
@@ -48,7 +71,7 @@ $query = array
         </tr>
       </thead>
       <tbody>
-        <?php foreach ($query as $row): ?>
+        <?php foreach ($db->query("SELECT silo_number, amount FROM public.sugar_silo") as $row): ?>
         <tr>
           <td>Silo <?=$row['silo_number']?></td>
           <td><?=$row['amount']?></td>
