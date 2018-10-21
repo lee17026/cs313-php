57c0e4980c32ba7dbf4776d27d96bb3949822637
Tim Lee <lee17026@byui.edu>
2018-10-20 20:27:00 -0700

    track.php I FORGOT TO ACTUALLY CONNECT TO THE DATABASE

--- web/week05/track.php
+++ web/week05/track.php
@@ -54,6 +54,26 @@ $filename=$_SERVER["PHP_SELF"];
     <?php if ($_SERVER['REQUEST_METHOD'] == 'POST'): ?>
     <?php
     $sugar_batch = htmlspecialchars($_POST['code']);
+	
+	try {
+	$dbUrl = getenv('DATABASE_URL');
+	
+	$dbOpts = parse_url($dbUrl);
+	
+	$dbHost = $dbOpts["host"];
+	$dbPort = $dbOpts["port"];
+	$dbUser = $dbOpts["user"];
+	$dbPassword = $dbOpts["pass"];
+	$dbName = ltrim($dbOpts["path"],'/');
+	
+	$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch (PDOException $ex) {
+	$msg = $ex->getMessage();
+	echo "Error!: $msg";
+	die();
+	}
 	/* dummy data for testing
     $query = array
       (
