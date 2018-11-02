a74aa60deffe518a1fe4b252af493124fdeca7f2
Tim Lee <lee17026@byui.edu>
2018-11-01 17:08:20 -0700

    prove 07 check for no rows

--- web/week07/track.php
+++ web/week07/track.php
@@ -97,6 +97,16 @@ if (!is_loggedin()) {
 		  ";
 		  die();
 	}
+	if (pg_num_rows($statement) <= 0) {
+		// row not found!
+		echo "
+		  <div class='alert alert-danger alert-dismissible fade show'>
+			<button type='button' class='close' data-dismiss='alert'>&times;</button>
+			<strong>No Such Batch!</strong> No batch found with code $sugar_batch. Please try a valid batch code.
+		  </div>
+		  ";
+		  die();
+	}
     ?>
     <div class="container">
       <h1>All Batches Mixed with Batch Code <?=$sugar_batch?></h1>
